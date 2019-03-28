ff5d778173972fd162da8b764a30892dca541b88
sergiomora03 <48146957+sergiomora03@users.noreply.github.com>
2019-03-28 00:00:38 -0500

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,35 @@
+library(shiny)
+
+# Define UI for slider demo application
+shinyUI(pageWithSidebar(
+  
+  #  Application title
+  headerPanel("Intertemporal Choice Task v.01"),
+  
+  sidebarPanel(
+    # This is intentionally an empty object.
+    h6(textOutput("save.results")),
+    h5("Created by:"),
+    tags$a("Analytical & Technology Department of the Politécnico Grancolombiano", 
+           href="https://www.poli.edu.co/"), #este link o otro?
+    h5("For details on how data is generated:"),
+    tags$a("Contact: Mora, S.", 
+           href="https://co.linkedin.com/in/sergiomorapardo"),
+    h5("Github Repository:"),
+    tags$a("Survey implementation", 
+           href=paste0("https://github.com/sergiomora03/",
+                       "survey")),
+    # Display the page counter text.
+    h5(textOutput("counter"))
+  ),
+  
+  
+  # Show a table summarizing the values entered
+  mainPanel(
+    # Main Action is where most everything is happenning in the
+    # object (where the welcome message, survey, and results appear)
+    uiOutput("MainAction"),
+    # This displays the action putton Next.
+    actionButton("Click.Counter", "Next")    
+  )
+))
